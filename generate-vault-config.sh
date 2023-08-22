7d84bad3c6ea03e88f05a2fdab0cbea45ee9895a
Amie Wei <amie.wei@hashicorp.com>
2023-08-22 13:48:13 -0500

    add generate-vault-config.sh

---
+++ generate-vault-config.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# get local ip
+HOSTIP=$(ipconfig getifaddr en0)
+
+# # Create the directory if it doesn't exist
+# mkdir -p config/vault/config
+
+# Create the vault.config file
+cat <<EOL > config/vault/config/vault.hcl
+storage "raft" {
+  path    = "/vault/file"
+  node_id = "node1"
+}
+
+listener "tcp" {
+  address     = "0.0.0.0:8200"
+  tls_disable = 1
+}
+
+api_addr     = "http://${HOSTIP}:8200"
+cluster_addr = "http://${HOSTIP}:8201"
+ui           = true
+EOL
+
+echo "List files in /vault/config after generation:"
+ls -l config/vault/config
